cae2b1fb7f12d1c6f5750171eba15fcdef38f04c
ayyleesh <xxserpentxbizzarexx@yahoo.co.id>
2019-08-13 20:19:35 +0100

    Tutorial and level up reward

---
+++ Assets/Scripts/GlobalPopUp.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalPopUp : MonoBehaviour
+{
+    public StatusBarScript statusBarScript;
+    public int currentLevel, newLevel;
+    public GameObject tutorialPopUp, RewardPrefab, instRewardPopUp;
+
+    private void Start()
+    {
+        currentLevel = PlayerPrefs.GetInt("currentLevel");
+        if (tutorialPopUp != null && PlayerPrefs.GetInt("tutorialIsShown") != 1)
+        {
+            ShowTutorial();
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if ( currentLevel != 0 && PlayerPrefs.GetInt("currentLevel") > currentLevel)
+        {
+            currentLevel = newLevel;
+            StartCoroutine(ShowLevelUp());
+        }
+    }
+
+    void ShowTutorial()
+    {
+        tutorialPopUp.SetActive(true);
+        PlayerPrefs.SetInt("tutorialIsShown", 1);
+    }
+
+    public void CloseTutorial()
+    {
+        if (tutorialPopUp.activeInHierarchy)
+        {
+            tutorialPopUp.SetActive(false);
+        }
+    }
+
+    IEnumerator ShowLevelUp()
+    {
+        yield return new WaitForSeconds(0.5f);
+        instRewardPopUp = Instantiate(RewardPrefab, GameObject.Find("Canvas").transform);
+        GameObject.Find("Panel Top").transform.GetChild(1).GetComponent<Text>().text = "You've reached level " + PlayerPrefs.GetInt("currentLevel");
+        Button closeButton = GameObject.Find("Panel Bottom").transform.GetChild(0).GetComponent<Button>();
+        PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt("coins") + 1);
+        closeButton.onClick.AddListener(() => CloseLevelUp());
+    }
+
+    public void CloseLevelUp()
+    {
+        Destroy(instRewardPopUp);
+    }
+
+}
--- Assets/Scripts/SongSelect/StatusBarScript.cs
+++ Assets/Scripts/SongSelect/StatusBarScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class StatusBarScript : MonoBehaviour
 {
+    public static StatusBarScript instance;
+
     public int expPointsTotal, currentLevel;
     public float levelPercent;
     int[] levelMaxExp = new int[] { 10000, 20000, 30000, 50000, 80000};
@@ -13,6 +15,7 @@ public class StatusBarScript : MonoBehaviour
 
     void Start()
     {
+        instance = this;
         expPointsTotal = PlayerPrefs.GetInt("expPoints");
         expSlider = FindObjectOfType<Slider>();
 
@@ -23,15 +26,21 @@ public class StatusBarScript : MonoBehaviour
                 currentLevel++;
             }
         }
+        PlayerPrefs.SetInt("currentLevel", currentLevel);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        levelText.text = "Lv. " + currentLevel;
+        levelText.text = "Lv. " + PlayerPrefs.GetInt("currentLevel");
         levelPercent = expPointsTotal * 100 / levelMaxExp[currentLevel];
         expSlider.value = levelPercent;
         coinsTotalText.text = "" + PlayerPrefs.GetInt("coins");
+    }
+
+    void LevelCount()
+    {
+
     }
 }
